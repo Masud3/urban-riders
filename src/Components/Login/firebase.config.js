d2fa78d09bcb1c43bc66c82488d29391259f3505
Masud3 <mdamdadulm79@gmail.com>
2021-03-21 07:09:56 -0800

    added all app

--- src/Components/Login/firebase.config.js
+++ src/Components/Login/firebase.config.js
@@ -1,5 +1,10 @@
 const firebaseConfig = {
-   
+  apiKey: "AIzaSyBT0wcsRoIZpp-bkOR6RWk9jfQnCRoKA3g",
+  authDomain: "react-auth-69b07.firebaseapp.com",
+  projectId: "react-auth-69b07",
+  storageBucket: "react-auth-69b07.appspot.com",
+  messagingSenderId: "97042875973",
+  appId: "1:97042875973:web:120630629241bb9357452c"
   };
 
   export default firebaseConfig;
